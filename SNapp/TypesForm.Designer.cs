cfe5a3955a1d9aa0406c0e3bb565ef8fd440f438
Aleksei Zhuravkov <aleksei_zhuravkov@epam.com>
2018-02-15 16:31:12 +0400

    Add types data to the form. New record and Save functionality implemented

--- SNapp/TypesForm.Designer.cs
+++ SNapp/TypesForm.Designer.cs
@@ -29,10 +29,280 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TypesForm));
+            this.sPORTNUTRITIONDataSet = new SNapp.SPORTNUTRITIONDataSet();
+            this.typesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.typesTableAdapter = new SNapp.SPORTNUTRITIONDataSetTableAdapters.TypesTableAdapter();
+            this.tableAdapterManager = new SNapp.SPORTNUTRITIONDataSetTableAdapters.TableAdapterManager();
+            this.typesBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.typesBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.typesDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.newRecord = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sPORTNUTRITIONDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.typesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.typesBindingNavigator)).BeginInit();
+            this.typesBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.typesDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sPORTNUTRITIONDataSet
+            // 
+            this.sPORTNUTRITIONDataSet.DataSetName = "SPORTNUTRITIONDataSet";
+            this.sPORTNUTRITIONDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // typesBindingSource
+            // 
+            this.typesBindingSource.DataMember = "Types";
+            this.typesBindingSource.DataSource = this.sPORTNUTRITIONDataSet;
+            // 
+            // typesTableAdapter
+            // 
+            this.typesTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.AddressTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.BrandsTableAdapter = null;
+            this.tableAdapterManager.CustomersTableAdapter = null;
+            this.tableAdapterManager.OrderDetailsTableAdapter = null;
+            this.tableAdapterManager.OrderListTableAdapter = null;
+            this.tableAdapterManager.OrdersTableAdapter = null;
+            this.tableAdapterManager.PaymentTableAdapter = null;
+            this.tableAdapterManager.ProductsTableAdapter = null;
+            this.tableAdapterManager.TypesTableAdapter = this.typesTableAdapter;
+            this.tableAdapterManager.UpdateOrder = SNapp.SPORTNUTRITIONDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // typesBindingNavigator
+            // 
+            this.typesBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.typesBindingNavigator.BindingSource = this.typesBindingSource;
+            this.typesBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.typesBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.typesBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.typesBindingNavigatorSaveItem});
+            this.typesBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.typesBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.typesBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.typesBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.typesBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.typesBindingNavigator.Name = "typesBindingNavigator";
+            this.typesBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.typesBindingNavigator.Size = new System.Drawing.Size(413, 25);
+            this.typesBindingNavigator.TabIndex = 0;
+            this.typesBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 22);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // typesBindingNavigatorSaveItem
+            // 
+            this.typesBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.typesBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("typesBindingNavigatorSaveItem.Image")));
+            this.typesBindingNavigatorSaveItem.Name = "typesBindingNavigatorSaveItem";
+            this.typesBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 22);
+            this.typesBindingNavigatorSaveItem.Text = "Save Data";
+            this.typesBindingNavigatorSaveItem.Click += new System.EventHandler(this.typesBindingNavigatorSaveItem_Click);
+            // 
+            // typesDataGridView
+            // 
+            this.typesDataGridView.AutoGenerateColumns = false;
+            this.typesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.typesDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.typesDataGridView.DataSource = this.typesBindingSource;
+            this.typesDataGridView.Location = new System.Drawing.Point(12, 28);
+            this.typesDataGridView.Name = "typesDataGridView";
+            this.typesDataGridView.Size = new System.Drawing.Size(244, 220);
+            this.typesDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "typeID";
+            this.dataGridViewTextBoxColumn1.HeaderText = "typeID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "type_description";
+            this.dataGridViewTextBoxColumn2.HeaderText = "type_description";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // newRecord
+            // 
+            this.newRecord.Location = new System.Drawing.Point(12, 255);
+            this.newRecord.Name = "newRecord";
+            this.newRecord.Size = new System.Drawing.Size(116, 23);
+            this.newRecord.TabIndex = 2;
+            this.newRecord.Text = "Новая запись";
+            this.newRecord.UseVisualStyleBackColor = true;
+            this.newRecord.Click += new System.EventHandler(this.newRecord_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(134, 255);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(121, 23);
+            this.save.TabIndex = 3;
+            this.save.Text = "Сохранить";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // TypesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "TypesForm";
+            this.ClientSize = new System.Drawing.Size(413, 313);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.newRecord);
+            this.Controls.Add(this.typesDataGridView);
+            this.Controls.Add(this.typesBindingNavigator);
+            this.Name = "TypesForm";
+            this.Text = "Менеджер типов";
+            this.Load += new System.EventHandler(this.TypesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sPORTNUTRITIONDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.typesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.typesBindingNavigator)).EndInit();
+            this.typesBindingNavigator.ResumeLayout(false);
+            this.typesBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.typesDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private SPORTNUTRITIONDataSet sPORTNUTRITIONDataSet;
+        private System.Windows.Forms.BindingSource typesBindingSource;
+        private SPORTNUTRITIONDataSetTableAdapters.TypesTableAdapter typesTableAdapter;
+        private SPORTNUTRITIONDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator typesBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton typesBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView typesDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Button newRecord;
+        private System.Windows.Forms.Button save;
     }
 }
--- SNapp/TypesForm.cs
+++ SNapp/TypesForm.cs
@@ -16,5 +16,31 @@ namespace SNapp
         {
             InitializeComponent();
         }
+
+        private void typesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.typesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.sPORTNUTRITIONDataSet);
+
+        }
+
+        private void TypesForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sPORTNUTRITIONDataSet.Types' table. You can move, or remove it, as needed.
+            this.typesTableAdapter.Fill(this.sPORTNUTRITIONDataSet.Types);
+
+        }
+
+        private void newRecord_Click(object sender, EventArgs e)
+        {
+            typesBindingSource.AddNew();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            typesBindingSource.EndEdit();
+            typesTableAdapter.Update(sPORTNUTRITIONDataSet);
+        }
     }
 }
